4136cd56479b5cc3b1b022dbe0e5e8d073c843ae
dinhhau2003 <nguyendinhhauace@gmail.com>
2025-03-31 21:57:57 +0700

    tao du an back end

---
+++ Back-end/quan_ly_cafe/src/test/java/com/example/quan_ly_cafe/QuanLyCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quan_ly_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
